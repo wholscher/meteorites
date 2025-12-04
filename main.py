80d798f3f8bf6bfb77db7ce6ec431b3c9b655e5c
wholscher <holscherwill@gmail.com>
2025-12-03 23:30:59 -0600

    Added GUI, player icon, the ability to draw circles

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    dt = 0
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width:", SCREEN_WIDTH)
     print(f"Screen height:",SCREEN_HEIGHT)
@@ -16,7 +20,11 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60)/1000
+
 
 if __name__ == "__main__":
     main()
